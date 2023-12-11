a466fe2638379dbcf84b322fe522523f87345ea9
dhva59 <146871627+dhva-2002@users.noreply.github.com>
2023-12-11 22:54:19 +0000

    Layout updates

--- app/src/main/java/org/feup/apm/aircarelocal/HistoryActivity.java
+++ app/src/main/java/org/feup/apm/aircarelocal/HistoryActivity.java
@@ -60,7 +60,7 @@ public class HistoryActivity extends AppCompatActivity {
 
 
         RecyclerView recyclerView = findViewById(R.id.historyList);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
 
         List<HistoryScroll.Item> itemList = fetchItemFromDatabase();
         HistoryScroll adapter = new HistoryScroll(itemList);
