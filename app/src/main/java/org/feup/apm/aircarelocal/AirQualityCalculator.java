5b50a900fb03745e29c1a58a2808f8deac5f8eaf
ritita02 <rita.marques2002@gmail.com>
2023-12-11 18:48:28 +0000

    CO2 to CO info on Parameter Details

--- app/src/main/java/org/feup/apm/aircarelocal/AirQualityCalculator.java
+++ app/src/main/java/org/feup/apm/aircarelocal/AirQualityCalculator.java
@@ -4,7 +4,7 @@ public class AirQualityCalculator {
 
     private static final double[] PM25_THRESHOLDS = {12.0, 35.0, 50.0};
     private static final double[] PM10_THRESHOLDS = {50, 150, 250};
-    private static final double[] CO_THRESHOLDS = {700, 1000, 2000};
+    private static final double[] CO_THRESHOLDS = {50, 100, 200};
     private static final double[] VOC_THRESHOLDS = {400, 2200, 30000};
 
     public enum AirQualityCategory {
